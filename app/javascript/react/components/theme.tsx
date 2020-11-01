3d0a8e55bf319dd517e9f22f0d5fedbf474ae443
Heidi Hood <heidihood@Heidis-MacBook-Pro.local>
2020-11-01 18:51:27 -0500

    style landing page

---
+++ app/javascript/react/components/theme.tsx
@@ -0,0 +1,16 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// Create a theme instance.
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#323954"
+    },
+    secondary: {
+      main: "#EA5147"
+    }
+  },
+  typography: {
+    fontFamily: 'Libre Baskerville'
+  },
+})
