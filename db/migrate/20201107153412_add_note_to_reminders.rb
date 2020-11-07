ebb7345c52fd377ec50ac80486f949e8e6aac3f3
Heidi Hood <heidihood@Heidis-MacBook-Pro.local>
2020-11-07 11:31:53 -0500

    add note field to reminder

---
+++ db/migrate/20201107153412_add_note_to_reminders.rb
@@ -0,0 +1,5 @@
+class AddNoteToReminders < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reminders, :note, :string 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_02_180147) do
+ActiveRecord::Schema.define(version: 2020_11_07_153412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_11_02_180147) do
     t.time "time", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "note"
     t.index ["user_id"], name: "index_reminders_on_user_id"
   end
 
