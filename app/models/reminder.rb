b0ed5174097c9eeb32e96f73ffbe74b8309d7415
Heidi Hood <heidih74@gmail.com>
2020-11-11 12:33:20 -0500

    add notes to sms reminders

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -5,13 +5,19 @@ class Reminder < ApplicationRecord
   validates :time, presence: true
 
   def send_sms 
+    phone_number = self.user.phone_number
+    formated_phone_number = phone_number.split("-").unshift("+1").join()
+
     @twilio_number = ENV['TWILIO_NUMBER']
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     @client = Twilio::REST::Client.new account_sid, ENV['TWILIO_AUTH_TOKEN']
     body = "Hi #{self.user.first_name} it's time to take your #{self.medication}!"
+    if self.note != ""
+      body.concat(" *Note: #{self.note}")
+    end
     message = @client.messages.create(
       :from => @twilio_number,
-      :to => '+14018356233',
+      :to => '+14018356233', #formated_phone_number
       :body => body,
     )
   end
